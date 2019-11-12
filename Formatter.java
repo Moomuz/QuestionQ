00b9aee3d0d995805a82984cec7d782195e8e8ce
Moomuz <36995948+Moomuz@users.noreply.github.com>
2019-11-11 17:05:59 -0800

    Add files via upload

---
+++ Formatter.java
@@ -0,0 +1,13 @@
+
+public class Formatter {
+	
+	public String formatQuestion(Queue queue, Question question)
+	{
+		return queue.indexOf(question)+1 + ": " + question.getQuestion(); //add 1 since list starts at 0
+	}
+	
+	public String formatStudent(Student student)
+	{
+		return "(" + student.getID() + ") " + student.getName();
+	}
+}
