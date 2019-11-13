bcad509337e4c28baaf73839468a3da340d19a80
Jen Luu <54510905+jenniferluu28@users.noreply.github.com>
2019-11-13 15:29:39 -0800

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -1,20 +1,51 @@
 import java.util.ArrayList;
 
+// A Queue that holds Question objects
 public class Queue {
-	private ArrayList<Question> questions;
+
+	private ArrayList<Question> queue;
 	
-	public Queue()
+	// Constructor to create the Queue
+	public Queue() 
 	{
-		questions = new ArrayList<Question>();
+		this.queue = new ArrayList<Question>();
+		
 	}
 	
+	// Add a Question to the Queue
 	public void addQuestion(Question question)
 	{
-		this.questions.add(question);
+		queue.add(question);
 	}
 	
-	public ArrayList<Question> getQueue()
+	// Return the first Question in the Queue
+	public String getFirstQuestion()
 	{
-		return this.questions;
+		return queue.get(0).getQuestion();
 	}
+	
+	// Return the last Question in the Queue
+	public String getLastQuestion()
+	{
+		return queue.get(queue.size()-1).getQuestion();
+	}
+	
+	// Return the ith Question in the Queue
+	public void deleteQuestion(int index)
+	{
+		queue.remove(index);
+	}
+	
+	// Print all of the Questions in the Queue
+	public void displayAll()
+	{
+		String question = "";
+		
+		for (int i = 0 ; i < queue.size(); i++)
+		{
+			question = queue.get(i).getQuestion();
+			System.out.println(i+1 + ". " + question);
+		}
+	}
+
 }
