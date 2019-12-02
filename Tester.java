1df638dd2fe9f4be5150abce52f99c9bd25a2f38
athena1999 <54375808+athena1999@users.noreply.github.com>
2019-12-01 19:15:11 -0800

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -3,6 +3,7 @@ import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 
 import javax.swing.*;
+import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 
 public class Tester {
@@ -17,6 +18,8 @@ public class Tester {
 		JFrame studentView = new JFrame();
 		JFrame profView = new JFrame();
 		JFrame queueView = new JFrame();
+		JFrame rosterView = new JFrame();
+		JFrame extraCreditView = new JFrame("Add extra credit");
 		
 		JLabel idLabel = new JLabel("Enter your student ID here (ex: XXXXXXXXX)");
 		studentView.add(idLabel);
@@ -47,6 +50,7 @@ public class Tester {
 			queue.addQuestion(question);
 			
 			JLabel qLabel = new JLabel(formatter.formatQuestion(queue, question));
+			
 			qLabels.add(qLabel);
 			
 			JPanel qPanel = new JPanel();
@@ -80,34 +84,23 @@ public class Tester {
 		profView.add(deleteField);
 		deleteField.addActionListener(event ->
 		{
-			//Turn user's Question number into an int value
 			String qPosition = deleteField.getText();
 			int qPos = 0;
 			qPos = Integer.parseInt(qPosition);
-			
-			//delete that question from queue
 			queue.deleteQuestion(qPos-1);
-			//get the label and panel with the question that wants to be deleted
 			JLabel toBeRemoved = qLabels.get(qPos-1);
 			JPanel qPanel = qPanels.get(qPos-1);
-			
-			//Set deleted panel's border to 0
-			qPanel.setBorder(new EmptyBorder(0, 0, 0, 0));
-			
-			//remove text question label from panel
+			qPanel.setBorder(new EmptyBorder(-8, 0, -8, 0));
 			qPanel.remove(toBeRemoved);
-			
-			//delete question from panel and label array list
+			profView.remove(qPanel);
 			qPanels.remove(qPos-1);
 			qLabels.remove(qPos-1);
 			
-			//update the number formatting of each question after deletion
 			for(int i =0; i< qLabels.size();i++)
 			{
 				Question currentQuestion = queue.getQuestion(i);
 				JLabel queueLabel = qLabels.get(i);
 				JPanel queuePanel = qPanels.get(i);
-				
 				queueLabel.setText(formatter.formatQuestion(queue, currentQuestion));
 				queuePanel.revalidate();
 				queuePanel.repaint();
@@ -120,6 +113,83 @@ public class Tester {
 		profView.setSize(400,300);
 		profView.setLocation(450, 0);
 		profView.setVisible(true);
+		
+		//Roster view frame
+		JLabel roster = new JLabel("Student Roster");
+		roster.setBorder(new EmptyBorder(0,(400-roster.getWidth())/2,0,(400-roster.getWidth())/2));
+		rosterView.add(roster);
+		Roster r = new Roster();
+		ArrayList<Student> students = r.getRoster();
+		
+		JPanel rosterPanel = new JPanel();
+		//GridLayout(rows, cols,horizontalGap,verticalGap)
+		rosterPanel.setLayout(new GridLayout(students.size()+2,2,250,15));
+		rosterPanel.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		//header
+		rosterPanel.add(new JLabel("SID"));
+		rosterPanel.add(new JLabel("EC"));
+		ArrayList<JLabel> ecLabels = new ArrayList<>();
+		for(int i = 0; i < students.size();i++)
+		{
+			Student s = students.get(i);
+			//turn student ID and extra credit into string so it can be put into a JLabel
+			String ID = String.valueOf(s.getID());
+			String EC = String.valueOf(s.getExtraCredit());
+			
+			JLabel studentID = new JLabel(ID);
+			JLabel ecLabel = new JLabel(EC);
+			rosterPanel.add(studentID);
+			rosterPanel.add(ecLabel);
+			ecLabels.add(ecLabel);
+		}
+		JScrollPane rosterScroll = new JScrollPane(rosterPanel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);  ;
+		rosterScroll.setPreferredSize(new Dimension(370,250));
+		rosterView.add(rosterScroll);
+		
+		//Extra credit view frame
+		JLabel ecStudentIDLabel = new JLabel("Enter Student ID: ");
+		extraCreditView.add(ecStudentIDLabel);
+		JTextField studentIDField = new JTextField(30);
+		extraCreditView.add(studentIDField);
+		JLabel ecNumLabel = new JLabel("Enter extra credit amount: ");
+		extraCreditView.add(ecNumLabel);
+		JTextField ecField = new JTextField(30);
+		extraCreditView.add(ecField);
+		ecField.addActionListener(event -> 
+		{
+			//get professor input for ID and extra credit amount
+			String textID = studentIDField.getText();
+			int SID = Integer.parseInt(textID);
+			String textEC = ecField.getText();
+			int EC = Integer.parseInt(textEC);
+			
+			//Get the student from professor's ID input
+			Student s = r.findStudent(SID);
+			//update the JLabel extra credit for the student
+			int index = students.indexOf(s);
+			JLabel updatedECLabel = ecLabels.get(index);
+			updatedECLabel.setText(textEC);
+			//update Roster.txt
+			r.inputExtraCredit(SID, EC);
+			
+			//repaint
+			rosterPanel.revalidate();
+			rosterPanel.repaint();
+		});
+		
+		extraCreditView.setLayout(new FlowLayout());
+		extraCreditView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		extraCreditView.pack();
+		extraCreditView.setSize(400,300);
+		extraCreditView.setLocation(0, 300);
+		extraCreditView.setVisible(true);
+		
+		rosterView.setLayout(new FlowLayout());
+		rosterView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		rosterView.pack();
+		rosterView.setSize(400,300);
+		rosterView.setLocation(450, 300);
+		rosterView.setVisible(true);
 		
 		queueView.setLayout(new FlowLayout());
 		queueView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
