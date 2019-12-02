222133da1ef70183230f99b1422074290e953480
athena1999 <54375808+athena1999@users.noreply.github.com>
2019-12-01 20:12:52 -0800

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -3,12 +3,12 @@ import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 
 import javax.swing.*;
+import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 
 public class Tester {
 	private static final int QUESTION_HEIGHT = 50;
 	private static final int QUESTION_WIDTH = 400;
-	private static final int ERROR_WIDTH = 150;
 	
 	public static void main(String[] args)
 	{
@@ -18,12 +18,14 @@ public class Tester {
 		JFrame studentView = new JFrame();
 		JFrame profView = new JFrame();
 		JFrame queueView = new JFrame();
+		JFrame rosterView = new JFrame();
+		JFrame extraCreditView = new JFrame("Add extra credit");
 		
-		JLabel idLabel = new JLabel("Enter your 9-digit student ID here");
+		JLabel idLabel = new JLabel("Enter your student ID here (ex: XXXXXXXXX)");
 		studentView.add(idLabel);
 		JTextField idField = new JTextField(20);
 		studentView.add(idField);
-		JLabel descLabel = new JLabel("Enter your question here");
+		JLabel descLabel = new JLabel("Enter your question here (Limit: 50 characters)");
 		studentView.add(descLabel);
 		JTextField qField = new JTextField(30);
 		studentView.add(qField);
@@ -31,158 +33,82 @@ public class Tester {
 		ArrayList<JLabel> qLabels = new ArrayList<>();
 		ArrayList<JPanel> qPanels = new ArrayList<>();
 		
-		//Student inputs new question
 		qField.addActionListener(event ->
 		{
 			String qString = qField.getText();
 			String idString = idField.getText();
 			int id = 0;
-			boolean IDIsValid = true;
-			//parse String input to int 
 	    	try 
 	    	{
 	    		id = Integer.parseInt(idString);
 	    	}
-	    	//prints error message if invalid ID is inputted
 	    	catch (Exception e)
 	    	{
-	    		IDIsValid = false;
-	    		JFrame errorPopUp = new JFrame();
-	    		JLabel errorLabel = new JLabel("Please enter a valid student ID");
-	    		JPanel errorPanel = new JPanel();
-	    		EmptyBorder errorBorder = new EmptyBorder(20, (ERROR_WIDTH-errorLabel.getWidth())/2, 0, (ERROR_WIDTH-errorLabel.getWidth())/2); //centers the error message
-	   	        errorPanel.setBorder(errorBorder);
-	   	        errorPanel.add(errorLabel);
-	   	        errorPopUp.add(errorPanel);
-	   	        
-	   	        errorPopUp.setLayout(new FlowLayout());
-	   	        errorPopUp.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-	   	        errorPopUp.pack();
-	   	        errorPopUp.setSize(250,150);
-	   			errorPopUp.setLocation(52, 52);
-	   	        errorPopUp.setVisible(true);
+	    		System.out.println("Please enter a valid student ID");
 	    	}
-	    	if(IDIsValid == true)
-	    	{
-				Question question = new Question(qString, id);
-				queue.addQuestion(question);
-				
-				//make new question label for queueView frame and adds to arraylist of question labels
-				JLabel qLabel = new JLabel(formatter.formatQuestion(queue, question));
-				qLabels.add(qLabel);
-				
-				JPanel qPanel = new JPanel();
-		        EmptyBorder panelBorder = new EmptyBorder(0, (QUESTION_WIDTH-qLabel.getWidth())/2, 0, (QUESTION_WIDTH-qLabel.getWidth())/2); //centers the question
-		        qPanel.setBorder(panelBorder);
-		        
-				qPanel.add(qLabel);
-				qPanels.add(qPanel);
-				
-				//add question to frame and repaints queueView frame
-				queueView.add(qPanel);
-				qLabel.setSize(new Dimension(QUESTION_WIDTH, QUESTION_HEIGHT));
-				queueView.repaint();
-				
-				queueView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				queueView.pack();
-				queueView.setSize(400,300);
-				queueView.setLocation(900, 0);
-				queueView.setVisible(true);
-			}
-    	});
+			Question question = new Question(qString, id);
+			queue.addQuestion(question);
+			
+			String formattedQuestion = formatter.formatQuestion(queue, question);
+			//JLabel qLabel = new JLabel(formatter.formatQuestion(queue, question));
+			JLabel qLabel = new JLabel();
+			qLabel.setText("<html><p>" + formattedQuestion + "</p><html>");
+			
+			qLabels.add(qLabel);
+			
+			JPanel qPanel = new JPanel();
+	        EmptyBorder panelBorder = new EmptyBorder(0, (400-qLabel.getWidth())/2, 0, (400-qLabel.getWidth())/2); //centers the question
+	        qPanel.setBorder(panelBorder);
+	        
+			qPanel.add(qLabel);
+			qPanels.add(qPanel);
+			
+			queueView.add(qPanel);
+			qLabel.setSize(new Dimension(QUESTION_WIDTH, QUESTION_HEIGHT));
+			queueView.repaint();
+			
+			queueView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			queueView.pack();
+			queueView.setSize(400,300);
+			queueView.setLocation(900, 0);
+			queueView.setVisible(true);
+		});
+
+		studentView.setLayout(new FlowLayout());
+		studentView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		studentView.pack();
+		studentView.setSize(400,300);
+		studentView.setVisible(true);
 		
-		//Professor deletes question from queue
+		
 		JLabel qNum = new JLabel("Delete a question:");
 		profView.add(qNum);
 		JTextField deleteField = new JTextField(30);
 		profView.add(deleteField);
 		deleteField.addActionListener(event ->
 		{
-			//Turn user's Question number into an int value
 			String qPosition = deleteField.getText();
 			int qPos = 0;
-			boolean questionNumberIsValid = true;
-			//parse String input to int 
-	    	try 
-	    	{
-	    		qPos = Integer.parseInt(qPosition);
-	    	}
-	    	//prints error message if invalid question number is inputted
-	    	catch (Exception e)
-	    	{
-	    		questionNumberIsValid = false;
-	    		JFrame errorPopUp = new JFrame();
-	    		JLabel errorLabel = new JLabel("Please enter a valid question number");
-	    		JPanel errorPanel = new JPanel();
-	    		EmptyBorder errorBorder = new EmptyBorder(20, (ERROR_WIDTH-errorLabel.getWidth())/2, 0, (ERROR_WIDTH-errorLabel.getWidth())/2); //centers the error message
-	   	        errorPanel.setBorder(errorBorder);
-	   	        errorPanel.add(errorLabel);
-	   	        errorPopUp.add(errorPanel);
-	   	        
-	   	        errorPopUp.setLayout(new FlowLayout());
-	   	        errorPopUp.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-	   	        errorPopUp.pack();
-	   	        errorPopUp.setSize(250,150);
-	   			errorPopUp.setLocation(502, 52);
-	   	        errorPopUp.setVisible(true);
-	    	}
-	    	//inputted question number is outside queue index bound
-			if(qPos > qLabels.size())
+			qPos = Integer.parseInt(qPosition);
+			queue.deleteQuestion(qPos-1);
+			JLabel toBeRemoved = qLabels.get(qPos-1);
+			JPanel qPanel = qPanels.get(qPos-1);
+			qPanel.setBorder(new EmptyBorder(-8, 0, -8, 0));
+			qPanel.remove(toBeRemoved);
+			profView.remove(qPanel);
+			qPanels.remove(qPos-1);
+			qLabels.remove(qPos-1);
+			
+			for(int i =0; i< qLabels.size();i++)
 			{
-	    		questionNumberIsValid = false;
-	    		JFrame errorPopUp = new JFrame();
-	    		JLabel errorLabel = new JLabel("Please enter a valid question number");
-	    		JPanel errorPanel = new JPanel();
-	    		EmptyBorder errorBorder = new EmptyBorder(20, (ERROR_WIDTH-errorLabel.getWidth())/2, 0, (ERROR_WIDTH-errorLabel.getWidth())/2); //centers the error message
-	   	        errorPanel.setBorder(errorBorder);
-	   	        errorPanel.add(errorLabel);
-	   	        errorPopUp.add(errorPanel);
-	   	        
-	   	        errorPopUp.setLayout(new FlowLayout());
-	   	        errorPopUp.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-	   	        errorPopUp.pack();
-	   	        errorPopUp.setSize(250,150);
-	   			errorPopUp.setLocation(502, 52);
-	   	        errorPopUp.setVisible(true);
-			}
-			//user enters valid question number, question is deleted from queue
-			else if(questionNumberIsValid == true)
-			{
-				//delete that question from queue
-				queue.deleteQuestion(qPos-1);
-				//get the label and panel with the question that wants to be deleted
-				JLabel toBeRemoved = qLabels.get(qPos-1);
-				JPanel qPanel = qPanels.get(qPos-1);
-				
-				//BRUTE FORCE LUL
-				qPanel.setBorder(new EmptyBorder(-8, 0, -8, 0));
-				
-				//remove text question label from panel
-				qPanel.remove(toBeRemoved);
-				
-				//delete question from panel and label array list
-				qPanels.remove(qPos-1);
-				qLabels.remove(qPos-1);
-				
-				//update the number formatting of each question after deletion
-				for(int i = 0; i < qLabels.size(); i++)
-				{
-					Question currentQuestion = queue.getQuestion(i);
-					JLabel queueLabel = qLabels.get(i);
-					JPanel queuePanel = qPanels.get(i);
-					
-					queueLabel.setText(formatter.formatQuestion(queue, currentQuestion));
-					queuePanel.revalidate();
-					queuePanel.repaint();
-				}
+				Question currentQuestion = queue.getQuestion(i);
+				JLabel queueLabel = qLabels.get(i);
+				JPanel queuePanel = qPanels.get(i);
+				queueLabel.setText(formatter.formatQuestion(queue, currentQuestion));
+				queuePanel.revalidate();
+				queuePanel.repaint();
 			}
 		});
-		
-		studentView.setLayout(new FlowLayout());
-		studentView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		studentView.pack();
-		studentView.setSize(400,300);
-		studentView.setVisible(true);
 		
 		profView.setLayout(new FlowLayout());
 		profView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -190,6 +116,99 @@ public class Tester {
 		profView.setSize(400,300);
 		profView.setLocation(450, 0);
 		profView.setVisible(true);
+		
+		JLabel roster = new JLabel("Student Roster");
+		roster.setBorder(new EmptyBorder(0,(400-roster.getWidth())/2,0,(400-roster.getWidth())/2));
+		rosterView.add(roster);
+		Roster r = new Roster();
+		ArrayList<Student> students = r.getRoster();
+		
+		JPanel headerPanel = new JPanel();
+		headerPanel.setLayout(new GridLayout(1,2,260,0));
+		headerPanel.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		headerPanel.add(new JLabel("Student ID"));
+		headerPanel.add(new JLabel("EC"));
+		rosterView.add(headerPanel);
+		
+		JPanel rosterContainer = new JPanel();
+		rosterContainer.setLayout(new GridLayout(1,2,5,0));
+		
+		JPanel rosterPanel = new JPanel();
+		rosterPanel.setLayout(new GridLayout(students.size(),2,90,15));
+		rosterPanel.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		
+		//rosterPanel.add(new JLabel("SID"));
+		//rosterPanel.add(new JLabel("EC"));
+		ArrayList<JLabel> ecLabels = new ArrayList<>();
+		for(int i = 0; i < students.size();i++)
+		{
+			Student s = students.get(i);
+			//int ID = s.getID();
+			String ID = String.valueOf(s.getID());
+			String EC = String.valueOf(s.getExtraCredit());
+			
+			JLabel studentID = new JLabel(ID);
+			JLabel ecLabel = new JLabel(EC);
+			rosterPanel.add(studentID);
+			rosterPanel.add(ecLabel);
+			ecLabels.add(ecLabel);
+			JButton addEC = new JButton("+");
+			addEC.setPreferredSize(new Dimension(10,10));
+			rosterPanel.add(addEC);
+			addEC.addActionListener(event ->
+			{
+				s.incrementEC();
+				String updatedEC = String.valueOf(s.getExtraCredit());
+				ecLabel.setText(updatedEC);
+				r.inputExtraCredit(s.getID(), s.getExtraCredit());
+				rosterPanel.revalidate();
+				rosterPanel.repaint();
+			});
+		}
+		JScrollPane rosterScroll = new JScrollPane(rosterPanel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);  ;
+		rosterScroll.setPreferredSize(new Dimension(380,230));
+		rosterView.add(rosterScroll);
+		//rosterView.add(rosterPanel);
+		
+		JLabel ecStudentIDLabel = new JLabel("Enter Student ID: ");
+		extraCreditView.add(ecStudentIDLabel);
+		JTextField studentIDField = new JTextField(30);
+		extraCreditView.add(studentIDField);
+		JLabel ecNumLabel = new JLabel("Enter extra credit amount: ");
+		extraCreditView.add(ecNumLabel);
+		JTextField ecField = new JTextField(30);
+		extraCreditView.add(ecField);
+		ecField.addActionListener(event -> 
+		{
+			String textID = studentIDField.getText();
+			int SID = Integer.parseInt(textID);
+			String textEC = ecField.getText();
+			int EC = Integer.parseInt(textEC);
+			
+			Student s = r.findStudent(SID);
+			int index = students.indexOf(s);
+			JLabel updatedECLabel = ecLabels.get(index);
+			updatedECLabel.setText(textEC);
+			r.inputExtraCredit(SID, EC);
+			
+			rosterPanel.revalidate();
+			rosterPanel.repaint();
+			rosterView.repaint();
+		});
+		
+		extraCreditView.setLayout(new FlowLayout());
+		extraCreditView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		extraCreditView.pack();
+		extraCreditView.setSize(400,300);
+		extraCreditView.setLocation(0, 300);
+		extraCreditView.setVisible(true);
+		
+		rosterView.setLayout(new FlowLayout());
+		rosterView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		rosterView.pack();
+		rosterView.setSize(400,300);
+		rosterView.setLocation(450, 300);
+		rosterView.setVisible(true);
 		
 		queueView.setLayout(new FlowLayout());
 		queueView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
