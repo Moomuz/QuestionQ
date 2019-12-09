eaac573cb09c8bc5fa564c9ccb965b6592182f72
Moomuz <36995948+Moomuz@users.noreply.github.com>
2019-12-08 21:20:54 -0800

    Update Roster.java

--- Roster.java
+++ Roster.java
@@ -1,23 +1,56 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class Roster {
 	private ArrayList<Student> students;
-	private ArrayList<String> formattedStudents;
 	
-	public Roster(ArrayList<Student> students)
+	public Roster()
 	{
-		Formatter formatter = new Formatter();
-		this.students = students;
-		this.formattedStudents = new ArrayList<String>();
-		for(Student student: this.students)
-		{
-			formattedStudents.add(formatter.formatStudent(student));
-		}
+		students = new ArrayList<>();
 	}
 	
-	public ArrayList<String> getRoster()
+	//return an array of students that was retrieved from Roster.txt
+	public ArrayList<Student> getRoster()
 	{
-		return this.formattedStudents;
+		ArrayList<Integer> fileID = new ArrayList<Integer>();
+		ArrayList<Integer> fileEXC = new ArrayList<Integer>();
+		
+		BufferedReader br;
+		
+		// Extract information from text file 
+		try
+		{
+			br = new BufferedReader(new FileReader("Roster.txt"));
+			
+			// Read the first name that contains the headers
+			String headers = br.readLine();
+			// Read from the second line 
+			String line = br.readLine();
+			while(line != null)
+			{
+				// Part 1 is ID, part 2 is exc 
+				String[] placeholder = line.split(" ");
+				
+				int id = Integer.parseInt(placeholder[0]);
+				int extraCredit = Integer.parseInt(placeholder[1]);
+				
+				students.add(new Student(id,extraCredit));
+				
+				line = br.readLine();
+			}
+			
+			br.close();
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		return this.students;
 	}
 	
 	public Student findStudent(int ID)
@@ -29,7 +62,106 @@ public class Roster {
 				return student;
 			}
 		}
-		return new Student(); //if student is not found, return "This student does not exist"
+		Student nullStudent = null;
+		return nullStudent; //if student is not found, return "This student does not exist"
 	}
 	
+	// Opens a text file of the roster of the class (ID and extra credit)
+	// Reads the file and allows the user to input extra credit
+	// Update the roster
+	public static void inputExtraCredit(int studentID, int exc)
+	{
+		ArrayList<Integer> fileID = new ArrayList<Integer>();
+		ArrayList<Integer> fileEXC = new ArrayList<Integer>();
+		
+		BufferedReader br;
+		
+		// Extract information from text file 
+		try
+		{
+			br = new BufferedReader(new FileReader("Roster.txt"));
+			
+			// Read the first name that contains the headers
+			String headers = br.readLine();
+			// Read from the second line 
+			String line = br.readLine();
+			
+			while(line != null)
+			{
+				// Part 1 is ID, part 2 is exc 
+				String[] placeholder = line.split(" ");
+				
+				int id = Integer.parseInt(placeholder[0]);
+				int extraCredit = Integer.parseInt(placeholder[1]);
+				
+				fileID.add(id);
+				fileEXC.add(extraCredit);
+				
+				line = br.readLine();
+			}
+			
+			br.close();
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	
+		
+		
+		// Go through the studentID list to find the desired ID to input exc
+		boolean done = false;
+		int index = 0;
+		
+		while(done!= true)
+		{
+			if (studentID == fileID.get(index))
+			{
+				fileEXC.set(index, exc);
+				done = true;
+			}
+			else
+			{
+				index++;
+			}
+		}
+			
+		try
+		{
+		// Write as a text file again
+		File myFile = new File("Roster.txt");
+		FileWriter fw = new FileWriter(myFile.getAbsoluteFile());
+		BufferedWriter bw = new BufferedWriter(fw);
+		
+		// Rewrite header
+		String header = "StudentID EXC";
+		bw.write(header);
+
+		
+		// Rewrite columns for roster
+			for (int i = 0 ; i < fileID.size(); i++)
+			{
+				bw.newLine();
+				bw.write(fileID.get(i) + " " + fileEXC.get(i));
+			}
+		bw.close();
+		
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+	}	
+	
+	//return Integer ArrayList of student IDs
+	public ArrayList<Integer> getIntRoster()
+	{
+		ArrayList<Integer> studentIDs = new ArrayList<Integer>();
+		for(Student student: students)
+		{
+			studentIDs.add(student.getID());
+		}
+		return studentIDs;
+	}
 }
